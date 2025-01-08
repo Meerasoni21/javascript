ed7f9dff9a610f07b53c45be76716127fd966a69
Meera <108992153+Meerasoni21@users.noreply.github.com>
2025-01-08 13:33:15 +0000

    done

---
+++ 01_basic/01-variables.js
@@ -0,0 +1,23 @@
+const accountId = 12345
+let accoutntEmail = "meera@gmail.com"
+var accountPassword = "852369"
+accountCity = "Durg"
+let accountState
+
+
+// const = const variable do not change 
+// let = 
+// var
+
+
+// accountId = 2  not allowed
+
+console.log(accountId)
+
+/*
+Prefer not to use var 
+because of issue in block scop and functional scope
+*/
+
+
+console.table([accountId, accoutntEmail, accountPassword, accountCity, accountState])
